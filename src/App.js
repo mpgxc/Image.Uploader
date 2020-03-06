efd8d054a4893d84856ec05a89778fedae21f1d5
mpgxc <mpgx5.c@gmail.com>
2020-03-06 03:16:47 -0300

    add multi-files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import GlobalStyle from "./styles/global";
 import { Container, Content } from "./styles";
-import  Upload from './components/Upload'
+import Upload from "./components/Upload";
+import FileList from "./components/FileList";
+import { uniqueId } from "lodash";
+import filesize from "filesize";
+import API from "./services/api";
 
 export default () => {
+  const [updFiles, setUpdFiles] = useState([]);
+
+  function handleUpload(files) {
+    setUpdFiles(
+      // Estudar a diferança entre state.concate vs setState([...state, setSate]) vs setState(...state, setSate)
+      updFiles.concat(
+        files.map(file => ({
+          file,
+          id: uniqueId(),
+          name: file.name,
+          readablesize: filesize(file.size),
+          preview: URL.createObjectURL(file),
+          progress: 0,
+          uploaded: false,
+          error: false,
+          url: null
+        }))
+      )
+    );
+  }
+
+  function processUpload(uploadedFile) {
+    const data = new FormData();
+    data.append("file", uploadedFile.file, uploadedFile.name);
+    API.post("/posts", data, {
+      onUploadProgress: env => {
+        const prog = parseInt(Math.round(env.loaded * 100) / env.total);
+      }
+    });
+  }
+
   return (
     <Container>
       <GlobalStyle />
       <Content>
-        <Upload/>
+        <Upload onUpload={handleUpload} />
+        {!!updFiles.length && <FileList files={updFiles} />}
       </Content>
     </Container>
   );
---
+++ src/components/FileList/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import { Container, FileInfo, Preview } from "./styles";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+import { MdCheckCircle, MdError, MdLink } from "react-icons/md";
+
+export default ({ files }) => {
+  return (
+    <Container>
+      {files.map(item => (
+        <li key={item.id}>
+          <FileInfo>
+            <Preview src={item.preview} />
+            <div>
+              <strong>{item.name}</strong>
+              <span>
+                {item.readablesize}{" "}
+                {!!item.url && <button onClick={() => {}}>Excluir</button>}
+              </span>
+            </div>
+          </FileInfo>
+          <div>
+            {!item.uploaded && !item.error && (
+              <CircularProgressbar
+                styles={{
+                  root: { width: 24 },
+                  path: { stroke: "#77dd77" }
+                }}
+                strokeWidth={15}
+                percentage={item.progress}
+              />
+            )}
+            {item.url && (
+              <a
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+              </a>
+            )}
+
+            {item.uploaded && <MdCheckCircle size={24} color="#77dd77" />}
+            {item.error && <MdError size={24} color="#e57878" />}
+          </div>
+        </li>
+      ))}
+    </Container>
+  );
+};
