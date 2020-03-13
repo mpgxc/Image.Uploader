7ec8ec383e1717da753399c314fec51bf1dca977
Francisco Junior <franciscojn49@gmail.com>
2020-03-13 17:36:56 -0300

    news

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import GlobalStyle from "./styles/global";
+import * as fs from "fs";
 
 import {
   Container,
@@ -7,7 +8,8 @@ import {
   MainContent,
   PreviewContent,
   PreviewContainer,
-  StyleButton
+  StyleButton,
+  DisableButton
 } from "./styles";
 
 import Upload from "./components/Upload";
@@ -15,11 +17,15 @@ import FileList from "./components/FileList";
 
 import { uniqueId } from "lodash";
 import filesize from "filesize";
-// import API from "./services/api";
+
+import { RemoveBgError, removeBackgroundFromImageBase64, removeBackgroundFromImageUrl } from "remove.bg";
+
+import 'dotenv/config';
 
 export default () => {
   const [updFiles, setUpdFiles] = useState([]);
-  const [loading, setloading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [base64img, setBase64img] = useState('');
 
   function handleUpload(files) {
     setUpdFiles(
@@ -40,14 +46,27 @@ export default () => {
     );
   }
 
-  function handleProcess(env) {
-    env.preventDefault();
-    setloading(true);
-  }
-
   function handleDownload(env) {
     env.preventDefault();
-    setloading(true);
+    setLoading(true);
+  }
+
+  async function handleProcess(url) {
+    
+    try{
+      const result = await removeBackgroundFromImageUrl({
+        url,
+        apiKey: process.env_api_code.API_KEY,
+        size: "regular",
+      });
+
+      setBase64img(result.base64img);
+      setLoading(true);
+      console.log(result.base64img)
+    }catch(env){
+      const errors: Array<RemoveBgError> = env;
+      console.log(JSON.stringify(errors));
+    }
   }
 
   return (
@@ -57,6 +76,7 @@ export default () => {
         <MainContent>
           <PreviewContainer>
             <PreviewContent>
+
               {!!updFiles.length && (
                 <img
                   width="100%"
@@ -66,9 +86,22 @@ export default () => {
                 />
               )}
             </PreviewContent>
-            <StyleButton onClick={handleProcess}>Processar</StyleButton>
-            <PreviewContent>{!!loading && <h1>Opaaa</h1>}</PreviewContent>
-            <StyleButton onClick={handleDownload}>Baixar Imagem</StyleButton>
+            
+           
+            <StyleButton onClick={() =>{handleProcess('https://f.i.uol.com.br/fotografia/2018/06/26/15300052565b32070863b0d_1530005256_3x2_md.jpg')}}>Processar</StyleButton>
+      
+            <PreviewContent>
+              {!!loading && (
+                <img alt = "result_image" width="100%" height="100%" src={`data:image/jpeg;base64,${base64img}`} /> 
+              )}
+            </PreviewContent>
+            
+            {!!updFiles.length > 0 ? (
+               <StyleButton onClick={handleDownload}>Baixar Imagem</StyleButton>
+            ):(
+              <DisableButton>Processar</DisableButton>
+            )}
+
           </PreviewContainer>
           <UploadContent>
             <Upload onUpload={handleUpload} />
